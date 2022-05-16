3b9c1162ed054b45b7948d59649c4939a155ef20
Briankaiserx <Briankaise@mail.ru>
2022-05-16 22:28:12 +0400

    Hexlet

--- gradlew.bat
+++ gradlew.bat
@@ -14,7 +14,6 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
-
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
